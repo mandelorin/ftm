d83087ed8acb50ba2ae128a8e094404802002108
mandelorin <supermeatdelpasand2021@gmail.com>
2025-06-15 10:57:35 +0330

    Update find.go

--- find.go
+++ find.go
@@ -10,7 +10,10 @@ import (
 func main() {
 	url := "https://acm.account.sony.com/create_account/agreement?client_id=37351a12-3e6a-4544-87ff-1eaea0846de2&redirect_uri=https%3A%2F%2Felectronics.sony.com%3FauthRedirect%3Dtrue&nonce=379a1503-ab4b-4cb8-b7e2-29fdd8f56b9a&state=fcc72b7a-a317-4cc9-b230-42f92ec39236&scope=openid%20users&mode=signup"
 
-	resp, err := http.Get(url)
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36")
+	resp, err := client.Do(req)
 	if err != nil {
 		fmt.Println("HTTP error:", err)
 		return
